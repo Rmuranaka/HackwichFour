a78c80a3756326f9d3b2b13267a0535ff239b17b
Ryan Muranaka <Rmuranak@gmail.com>
2019-02-28 10:32:59 -1000

    Completed Hackwich4, Introduction to the Mighty Tableview

--- HackwichFour/ViewController.swift
+++ HackwichFour/ViewController.swift
@@ -8,7 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //    Declare an array and name it "myFriends"
+    var myFriends  = ["Darren","Shea","Ross", "Jesse"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myFriends.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //Create a constant called cell [local variable, only accessible in the function], each cell created it puts the name in the cell and continues the process
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        //takes each item in the array
+        let text = myFriends[indexPath.row]
+        //sets the cell to the text
+        //textLabel [property].text[property] = text
+        cell.textLabel?.text=text
+        //returns the cell
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
